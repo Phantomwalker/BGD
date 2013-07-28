b38d6d79ce1d1cce687dd9f4fba0d8893e66baf7
Phantomwalker <chris_webster@yahoo.com>
2013-07-28 02:30:01 -0600

    SO HUGE!

--- Assets/CubeScript.cs
+++ Assets/CubeScript.cs
@@ -10,6 +10,7 @@ public class CubeScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	transform.Rotate(0.0f, 1.1f, 0.0f);
+		transform.Rotate(0.0f, 1.1f, 0.0f);
+		transform.localScale = new Vector3(Time.time, Time.time, Time.time);
 	}
 }
