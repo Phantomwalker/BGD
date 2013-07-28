412a0af2c2e5ce8023341b0a1163112560b5919e
Phantomwalker <chris_webster@yahoo.com>
2013-07-28 02:37:05 -0600

    THE WORLD NEEDS MY HELP SON!

--- Assets/CubeScript.cs
+++ Assets/CubeScript.cs
@@ -5,7 +5,7 @@ public class CubeScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		transform.position = new Vector3(1, 2, 8);	
 	}
 	
 	// Update is called once per frame
